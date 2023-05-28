7a341f0d668f0ee03ae22f8136a5e80956d1bb20
Marek Kloza <kloza.marek@gmail.com>
2023-05-28 13:42:33 +0200

    remove unnecessary files

--- ape2flac.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-# script to convert .ape file to .flac file
-# run from the relevant directory
-
-find . -name "*.ape" -exec sh -c 'exec ffmpeg -i "$1" "${1%.ape}.flac"' _ {} \;
--- tag2flac.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-# script to tag flac files from cue sheet
-# remove first the unsplit .flac file
-
-find . -name "*.cue" -execdir sh -c 'exec cuetag.sh "$1" *.flac' _ {} \;
